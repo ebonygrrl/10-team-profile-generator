2e85385e69d1201b577d56f07a9cab0869116e03
ebonygrrl <abooker77@yahoo.com>
2022-12-04 01:53:20 -0600

    employee test init

---
+++ tests/Employee.test.js
@@ -0,0 +1,24 @@
+class Employee {
+    constructor(name,id,email,role) {
+        this.name  = name;
+        this.id    = id;
+        this.email = email;
+        this.role  = role;
+    }
+
+    getName() {
+        console.log(this.name);
+    }
+
+    getId() {
+        console.log(this.id);
+    }
+
+    getEmail() {
+        console.log(this.email);
+    }
+
+    getRole() {
+        console.log(this.role);
+    }
+}
