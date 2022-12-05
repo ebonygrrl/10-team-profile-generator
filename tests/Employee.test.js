e54ad610ed09c6dc0f85b061cb9392515a0e32ca
ebonygrrl <abooker77@yahoo.com>
2022-12-05 15:23:18 -0600

    employee test successfully passed

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -28,35 +28,40 @@ describe('Employee class', () => {
         });
     });
 
-    // describe('getId', () => {
-    //     it('returns true if number is valid', () => {
-    //         const staff = new Employee('Tori',1,'email@email.com');
+    describe('getId', () => {
+        it('returns true if number is 0-99', () => {
+            const id = 55;
+            const staff = new Employee('Tori',55,'email@email.com');
 
-    //         expect(staff.getId()).toBe(true);
-    //     });
-    //     it('returns false if number not valid', () => {
-    //         const staff = new Employee('tori booker',101,'email@email.com');
+            expect(staff.getId()).toBe(id);
+        });
+        it('returns false if number higher than 99', () => {
+            const id = 99;
+            const staff = new Employee('tori booker',101,'email@email.com');
 
-    //         expect(staff.getId()).toBe(false);
-    //     });
-    // });
+            expect(staff.getId()).toBe(id);
+        });
+    });
 
-    // describe('getEmail', () => {
-    //     it('returns true if email is valid', () => {
-    //         const staff = new Employee('Tori',1,'email@email.com');
+    describe('getEmail', () => {
+        it('returns email address if email is valid', () => {
+            const email = 'abooker77@yahoo.com';
+            const staff = new Employee('Tori',1,'abooker77@yahoo.com');
 
-    //         expect(staff.getEmail()).toBe(true);
-    //     });
-    //     it('returns false if email not valid', () => {
-    //         const staff = new Employee('Tori',1,'email@emailcom');
+            expect(staff.getEmail()).toBe(email);
+        });
+        it('returns error message if email is invalid', () => {
+            const error = 'Invalid email.';
+            const staff = new Employee('Tori',1,'email@emailcom');
 
-    //         expect(staff.getEmail()).toBe(false);
-    //     });
-    //     it('returns false if email not valid', () => {
-    //         const staff = new Employee('Tori',1,'emailemail.com');
+            expect(staff.getEmail()).toBe(error);
+        });
+        it('returns error message if email is invalid', () => {
+            const error = 'Invalid email.';
+            const staff = new Employee('Tori',1,'emailemail.com');
 
-    //         expect(staff.getEmail()).toBe(false);
-    //     });
-    // });
+            expect(staff.getEmail()).toBe(error);
+        });
+    });
 
 });
